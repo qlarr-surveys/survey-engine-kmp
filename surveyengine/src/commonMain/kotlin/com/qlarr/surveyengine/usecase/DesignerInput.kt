85540eb3abd777d5b97d8c780904026fa90e8ea8
Amr Askoura <askoura@qlarr..com>
2025-05-14 13:55:02 +0200

    remove useless

--- surveyengine/src/commonMain/kotlin/com/qlarr/surveyengine/usecase/DesignerInput.kt
+++
@@ -1,9 +0,0 @@
-package com.qlarr.surveyengine.usecase
-
-import kotlinx.serialization.json.JsonObject
-import com.qlarr.surveyengine.model.ComponentIndex
-
-data class DesignerInput(
-    val state: JsonObject,
-    val componentIndexList: List<ComponentIndex>
-)
