3375fc59393cacffe670699136d78b6ebebb1177
Amr Askoura <askoura@qlarr..com>
2025-05-18 20:55:59 +0200

    fix unit tests

--- surveyengine/src/jvmTest/kotlin/com/qlarr/surveyengine/usecase/NavigationUseCaseWrapperImplTest.kt
+++ surveyengine/src/jvmTest/kotlin/com/qlarr/surveyengine/usecase/NavigationUseCaseWrapperImplTest.kt
@@ -3,10 +3,14 @@ package com.qlarr.surveyengine.usecase
 import com.qlarr.scriptengine.getNavigate
 import com.qlarr.surveyengine.common.loadFromResources
 import com.qlarr.surveyengine.ext.engineScript
+import com.qlarr.surveyengine.model.NavigationJsonOutput
 import com.qlarr.surveyengine.model.exposed.NavigationDirection
 import com.qlarr.surveyengine.model.exposed.NavigationMode
 import com.qlarr.surveyengine.model.exposed.SurveyMode
+import com.qlarr.surveyengine.model.jsonMapper
+import kotlinx.serialization.json.jsonObject
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class NavigationUseCaseWrapperImplTest {
 
@@ -22,6 +26,11 @@ class NavigationUseCaseWrapperImplTest {
             navigationMode = NavigationMode.GROUP_BY_GROUP,
             navigationDirection = NavigationDirection.Start
         ).navigate()
-        println(output)
+        val deserialised: NavigationJsonOutput =
+            jsonMapper.decodeFromString(NavigationJsonOutput.serializer(), output)
+        assertTrue(deserialised.toSave.isNotEmpty())
+        assertTrue(deserialised.state["qlarrVariables"]!!.jsonObject.isNotEmpty())
+        assertTrue(deserialised.state["qlarrDependents"]!!.jsonObject.isNotEmpty())
+        assertTrue(deserialised.toSave.isNotEmpty())
     }
 }
--- surveyengine/src/jvmTest/kotlin/com/qlarr/surveyengine/usecase/ValidationUseCaseWrapperImplTest.kt
+++ surveyengine/src/jvmTest/kotlin/com/qlarr/surveyengine/usecase/ValidationUseCaseWrapperImplTest.kt
@@ -3,6 +3,7 @@ package com.qlarr.surveyengine.usecase
 import com.qlarr.scriptengine.getValidate
 import com.qlarr.surveyengine.common.loadFromResources
 import com.qlarr.surveyengine.model.jsonMapper
+import kotlinx.serialization.json.buildJsonObject
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -11,13 +12,13 @@ class ValidationUseCaseWrapperImplTest {
     @Test
     fun validate() {
         val x = loadFromResources("2.json")
-        println(x)
         val stringOutput = ValidationUseCaseWrapper.create(getValidate(), loadFromResources("1.json")).validate()
         val jsonOutput: ValidationJsonOutput =
             jsonMapper.decodeFromString(ValidationJsonOutput.serializer(), stringOutput)
         val stringified: String = jsonMapper.encodeToString(ValidationJsonOutput.serializer(), jsonOutput)
         val deserialised: ValidationJsonOutput =
             jsonMapper.decodeFromString(ValidationJsonOutput.serializer(), stringified)
-        assertEquals(jsonOutput, deserialised)
+        assertEquals(jsonOutput.survey, deserialised.survey)
+        assertEquals(jsonOutput.script, deserialised.script)
     }
 }
