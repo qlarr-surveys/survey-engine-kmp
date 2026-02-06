99777b66f4ff21a0fa02b8e02871a9d57b46d75c
Amr Askoura <askoura@qlarr..com>
2026-02-06 22:41:42 +0200

    feat: fault toleranto serialization

--- surveyengine/src/commonMain/kotlin/com/qlarr/surveyengine/usecase/ValidationUseCaseWrapperImpl.kt
+++ surveyengine/src/commonMain/kotlin/com/qlarr/surveyengine/usecase/ValidationUseCaseWrapperImpl.kt
@@ -1,7 +1,9 @@
 package com.qlarr.surveyengine.usecase
 
 import com.qlarr.surveyengine.ext.copyComponentsToJson
-import com.qlarr.surveyengine.model.Survey
+import com.qlarr.surveyengine.ext.copyDeserializationIssues
+import com.qlarr.surveyengine.model.DeserializationIssue
+import com.qlarr.surveyengine.model.TolerantSurveyComponentSerializer
 import com.qlarr.surveyengine.model.jsonMapper
 import com.qlarr.surveyengine.scriptengine.getValidate
 import kotlinx.serialization.json.jsonObject
@@ -9,19 +11,26 @@ import kotlinx.serialization.json.jsonObject
 internal class ValidationUseCaseWrapperImpl(private val surveyJson: String) :
     ValidationUseCaseWrapper {
     private val useCase: ValidationUseCase
+    private val deserializationIssues: List<DeserializationIssue>
 
     init {
-        val survey = jsonMapper.decodeFromString<Survey>(surveyJson)
-        useCase = ValidationUseCaseImpl(getValidate(), survey)
+        val result = TolerantSurveyComponentSerializer.deserializeTolerant(surveyJson)
+        useCase = ValidationUseCaseImpl(getValidate(), result.survey)
+        deserializationIssues = result.issues
     }
 
 
     override fun validate(): String {
         val output = useCase.validate()
         val survey = jsonMapper.parseToJsonElement(surveyJson).jsonObject
+
+        // First copy components (errors, instructions, etc.), then copy deserialization issues
+        val surveyWithComponents = output.survey.copyComponentsToJson(survey)
+        val surveyWithIssues = surveyWithComponents.copyDeserializationIssues(deserializationIssues)
+
         return jsonMapper.encodeToString(
             ValidationJsonOutput.serializer(), ValidationJsonOutput(
-                survey = output.survey.copyComponentsToJson(survey),
+                survey = surveyWithIssues,
                 schema = output.schema,
                 impactMap = output.impactMap,
                 componentIndexList = output.componentIndexList,
